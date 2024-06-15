bb9f49ba99681cce84f02035b953531fc9570742
Wisdom Maliki <wisdommaliki19@gmail.com>
2024-06-15 12:25:09 -0600

    add .env file

--- src/utilis/firebase.utils.js
+++ src/utilis/firebase.utils.js
@@ -23,12 +23,12 @@ import {
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB2COjPEhR2VmCWI6ZT5Qm54m7sYERea80",
-  authDomain: "samplestore-2b723.firebaseapp.com",
-  projectId: "samplestore-2b723",
-  storageBucket: "samplestore-2b723.appspot.com",
-  messagingSenderId: "1013745898454",
-  appId: "1:1013745898454:web:f9a11a82f72561e3444a77",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
