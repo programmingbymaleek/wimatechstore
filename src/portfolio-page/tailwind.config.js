c749ac4ca210641b183fceefe84b1e4ab3e5051b
Mary Cole <ciacoleman988@gmail.com>
2024-08-01 15:33:16 +0100

    created base folder

---
+++ src/portfolio-page/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      "2xs": "440px",
+      xs: "540px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      backgroundImage: {
+        "hero-backgroundImage": "url('./src/assets/image1.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
