a4be9876dd4ebd2184288a2a183ff5fd0f64ff55
Mary <adebimpecole02@gmail.com>
2024-08-08 11:39:40 +0100

    made the navbar sticky

--- portfolio-page/src/app/components/navbar-component/navbar.component.jsx
+++ portfolio-page/src/app/components/navbar-component/navbar.component.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import logo from "../../../../public/assets/images/globe-logo.png";
 import close from "../../../../public/assets/icons/close.svg";
 import bar from "../../../../public/assets/icons/menu-bar.svg";
@@ -10,63 +10,109 @@ import Image from "next/image";
 
 export const NavBarComponent = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
+  useEffect(() => {
+    const navbar = document.getElementById("navbar");
+    const stickyPoint = 52;
+
+    const handleScroll = () => {
+      if (window.scrollY >= stickyPoint) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header className="bg-transparent w-full">
-      <nav className="mx-auto flex flex-col max-w-7xl " aria-label="Global">
-        <div className="w-full bg-white h-[3.3rem] py-2 md:px-12 flex flex-row justify-between items-center">
-          <a href="#home" className="h-full flex flex-row gap-3 items-center">
-            <span className="sr-only">Your Company</span>
-            <Image src={logo} className="h-full w-[2.1rem]" alt="Logo" />
-            <p className="capitalize text-smaller font-semibold text-blue-900 w-24 leading-[0.9rem] mt-1">
-              Certified Global Enterprise
-            </p>
-          </a>
-          <Image src={flag} className="h-auto w-[1.5rem]" alt="Logo" />
-        </div>
-        <div className="py-5 md:px-12">
-          <div className="flex md:hidden">
-            <button className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white cursor-pointer hover:bg-white/20">
-              <span className="sr-only">Open main menu</span>
-              <Image
-                src={bar}
-                width={4}
-                height={4}
-                className="h-5 w-auto"
-                alt="menu bar icon"
-                onClick={toggleMenu}
-              />
-            </button>
+      <nav className=" flex flex-col w-full " aria-label="Global">
+        <div className=" bg-white h-[3.3rem] py-2 w-full ">
+          <div className="mx-auto max-w-7xl h-full flex flex-row justify-between items-center md:px-12 ">
+            <a href="#home" className="h-full flex flex-row gap-3 items-center">
+              <span className="sr-only">Your Company</span>
+              <Image src={logo} className="h-full w-[2.1rem]" alt="Logo" />
+              <p className="capitalize text-smaller font-semibold text-blue-900 w-24 leading-[0.9rem] mt-1">
+                Certified Global Enterprise
+              </p>
+            </a>
+            <Image src={flag} className="h-auto w-[1.5rem]" alt="Logo" />
           </div>
-          <div className="hidden md:flex md:flex-1 md:justify-end md:gap-12 uppercase">
+        </div>
+        <div
+          className={` w-full transition-all duration-300 z-50 ${
+            isSticky
+              ? "fixed top-0 bg-white shadow-md pb-3 pt-2"
+              : " bg-transparent py-4"
+          }`}
+        >
+          <div className="flex flex-row justify-between items-center md:px-12 mx-auto max-w-7xl w-full ">
             <a
               href="#home"
-              className="text-small font-medium leading-6 text-white hover:font-bold"
+              className={`h-full flex-row gap-3 items-center transition-all duration-300 z-50 ${
+                isSticky ? "flex" : "hidden"
+              }`}
             >
-              Home
+              <span className="sr-only">Your Company</span>
+              <Image src={logo} className="h-full w-[2.1rem]" alt="Logo" />
+              <p className="capitalize text-smaller font-semibold text-blue-900 w-24 leading-[0.9rem] mt-1">
+                Certified Global Enterprise
+              </p>
             </a>
-            <a
-              href="#aboutus"
-              className="text-small font-medium leading-6 text-white hover:font-bold"
+            <div className="flex md:hidden">
+              <button className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white cursor-pointer hover:bg-white/20">
+                <span className="sr-only">Open main menu</span>
+                <Image
+                  src={bar}
+                  width={4}
+                  height={4}
+                  className="h-5 w-auto"
+                  alt="menu bar icon"
+                  onClick={toggleMenu}
+                />
+              </button>
+            </div>
+            <div
+              className={`hidden md:flex md:flex-1 md:justify-end md:gap-12 uppercase transition-all duration-300 z-50 ${
+                isSticky ? "text-black mt-1" : "text-white mt-0"
+              }`}
             >
-              About
-            </a>
-            <a
-              href="#services"
-              className="text-small font-medium leading-6 text-white hover:font-bold"
-            >
-              Services
-            </a>
-            <a
-              href="#contactus"
-              className="text-small font-medium leading-6 text-white hover:font-bold"
-            >
-              Contact Us
-            </a>
+              <a
+                href="#home"
+                className="text-small font-medium leading-6 hover:font-bold"
+              >
+                Home
+              </a>
+              <a
+                href="#aboutus"
+                className="text-small font-medium leading-6 hover:font-bold"
+              >
+                About
+              </a>
+              <a
+                href="#services"
+                className="text-small font-medium leading-6 hover:font-bold"
+              >
+                Services
+              </a>
+              <a
+                href="#contactus"
+                className="text-small font-medium leading-6 hover:font-bold"
+              >
+                Contact Us
+              </a>
+            </div>
           </div>
         </div>
       </nav>
--- portfolio-page/src/app/layout.js
+++ portfolio-page/src/app/layout.js
@@ -1,50 +1,50 @@
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import "./globals.css";
 
 const gtPlanar = localFont({
   src: [
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Bold.otf',
-      weight: '700',
-      style: 'normal',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Bold.otf",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Medium.otf',
-      weight: '600',
-      style: 'normal',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Medium.otf",
+      weight: "600",
+      style: "normal",
     },
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Regular.otf',
-      weight: '500',
-      style: 'normal',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Regular.otf",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Light.otf',
-      weight: '400',
-      style: 'normal',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Light.otf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Thin.otf',
-      weight: '300',
-      style: 'normal',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Thin.otf",
+      weight: "300",
+      style: "normal",
     },
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Italic-15-Regular.otf',
-      weight: '500',
-      style: 'italic',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Italic-15-Regular.otf",
+      weight: "500",
+      style: "italic",
     },
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Italic-15-Light.otf',
-      weight: '400',
-      style: 'italic',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Italic-15-Light.otf",
+      weight: "400",
+      style: "italic",
     },
     {
-      path: '../../../../public/fonts/GT-Planar/GT-Planar-AC-Italic-15-Thin.otf',
-      weight: '300',
-      style: 'italic',
+      path: "../../../public/fonts/GT-Planar/GT-Planar-AC-Italic-15-Thin.otf",
+      weight: "300",
+      style: "italic",
     },
   ],
-})
+});
 
 export const metadata = {
   title: "Certified Global Enterprise",
